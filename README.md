d650b7256c0cfad5651f405acb95b82d382ba1b0
wwessel01 <wessel01@ziggo.nl>
2023-03-02 23:41:38 +0100

    init: template project for svelte + ts + tailwind

---
+++ README.md
@@ -0,0 +1,15 @@
+# Svelte + TS + Vite + TailwindCSS
+
+This template should help get you started developing with Svelte using TypeScript and TailwindCSS in Vite.
+
+## Getting started
+1. Fork the repo
+2. Run: `yarn`
+3. Run: `yarn dev`
+4. Start creating!
+
+TailwindCSS and Svelte-icons are already installed in this initial template.
+
+## Recommended IDE Setup
+
+[VS Code](https://code.visualstudio.com/) + [Svelte](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) + [TailwindCSS](https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss).
---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte'
+
+  const title = "Vite + Svelte + TypeScript + TailwindCSS";
+</script>
+
+<main class="w-screen h-screen flex flex-col gap-4 justify-center items-center p-5">
+  <h1 class="text-3xl font-bold text-center">{title}</h1>
+  <h2 class="text-2xl text-zinc-400 flex flex-col gap-2 md:flex-row">A starter template by 
+    <a target="_blank" rel="noreferrer" class="italic text-teal-500 flex justify-center items-center hover:scale-95 hover:text-teal-700 transition-all" href="https://github.com/wwessel01">
+      wwessel01
+      <div class="w-6 h-6 mt-1">
+        <DiGithubBadge />
+      </div>
+    </a>
+  </h2>
+</main>
